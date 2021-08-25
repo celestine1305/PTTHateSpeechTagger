ec932c8d316a9d18cdd0cde6b972c8bb0b66db4d
celestine1305 <celestine1305@gmail.com>
2021-08-26 05:24:35 +0800

    template

--- README.md
+++
@@ -1,7 +0,0 @@
-# README.md
-
-- clone this repo
-- 到 chrome://extensions/
-- 載入未封裝項目
-- 選 repo 資料夾
-- 按 popup icon 開關就可以ㄌ
---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+document.getElementById("tutorial").addEventListener("click", function () {
+    location.href = "./tutorial.html";
+});
+document.getElementById("home").addEventListener("click", function () {
+    location.href = "./home.html";
+});
+document.getElementById("rank").addEventListener("click", function () {
+    location.href = "./rank.html";
+});
+document.getElementById("blacklist").addEventListener("click", function () {
+    location.href = "./blacklist.html";
+});
+
+
